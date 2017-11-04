764adaff965d89f2b8855adb0fa98175590ef293
jtaylor351 <jtaylor351@gatech.edu>
2017-11-04 12:51:21 -0400

    created templates for each page

---
+++ wordpress-theme-work/footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+
+    <div class="wrap_1280">
+
+        <div class="clearfix">
+
+            <div class="copyright">
+
+                <p>© Copyright <?php echo date("Y"); ?> | GT Bits of Good | by GT Bits of Good</p>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
