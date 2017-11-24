a5f381fb8a2f261e159869faf14f3e3b9a2cd940
jtaylor351 <jtaylor351@gatech.edu>
2017-11-23 23:47:59 -0500

    improved responsiveness

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 function checkSize() {
-    return $(window).width() < 1200;
+    return $(window).width() > 1200;
 }
 
 $(document).ready(function() {
+    if (checkSize()) {
+        $("#logo-social").css("display", "inline-block");
+        $("#donate").css("display", "inline-block");
+
+        // $("#navbar").addClass("navnavbar-fixed-top");
+    } else {
+        $("#logo-social").css("display", "none");
+        $("#donate").css("display", "none");
+        // $("#navbar").removeClass("navnavbar-fixed-top");
+        // $("#navbar").addClass("navnavbar-top");
+    }
+
+
 
     // if (checkSize()) {
     //     $("#mobile-logo").css("display", "inline-block");
@@ -13,10 +26,13 @@ $(document).ready(function() {
     // logic for making nav bar responisve
     $(window).resize(function() {
         if (checkSize()) {
-            $("#mobile-logo").css("display", "inline-block");
+            $("#logo-social").css("display", "inline-block");
+            $("#donate").css("display", "inline-block");
+
             // $("#navbar").addClass("navnavbar-fixed-top");
         } else {
-            $("#mobile-logo").css("display", "none");
+            $("#logo-social").css("display", "none");
+            $("#donate").css("display", "none");
             // $("#navbar").removeClass("navnavbar-fixed-top");
             // $("#navbar").addClass("navnavbar-top");
         }
