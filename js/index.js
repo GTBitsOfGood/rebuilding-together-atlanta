729d618cfa618b6d571c6cf2fadd2b8798041a66
jtaylor351 <jtaylor351@gatech.edu>
2017-11-24 19:31:52 -0500

    standardized header

--- js/index.js
+++ js/index.js
@@ -1,42 +1,4 @@
-function checkSize() {
-    return $(window).width() > 1200;
-}
-
 $(document).ready(function() {
-    if (checkSize()) {
-        $("#logo-social").css("display", "inline-block");
-        $("#donate").css("display", "inline-block");
-
-        // $("#navbar").addClass("navnavbar-fixed-top");
-    } else {
-        $("#logo-social").css("display", "none");
-        $("#donate").css("display", "none");
-        // $("#navbar").removeClass("navnavbar-fixed-top");
-        // $("#navbar").addClass("navnavbar-top");
-    }
-
-
-
-    // if (checkSize()) {
-    //     $("#mobile-logo").css("display", "inline-block");
-    // } else {
-    //     $("#mobile-logo").css("display", "none");
-    // }
-
-    // logic for making nav bar responisve
-    $(window).resize(function() {
-        if (checkSize()) {
-            $("#logo-social").css("display", "inline-block");
-            $("#donate").css("display", "inline-block");
-
-            // $("#navbar").addClass("navnavbar-fixed-top");
-        } else {
-            $("#logo-social").css("display", "none");
-            $("#donate").css("display", "none");
-            // $("#navbar").removeClass("navnavbar-fixed-top");
-            // $("#navbar").addClass("navnavbar-top");
-        }
-    });
 
 
     // Handle section switching for content
