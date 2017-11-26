2c57cfb20ac775f34c2c7b83a18434b476975c8b
jtaylor351 <jtaylor351@gatech.edu>
2017-11-25 20:28:41 -0500

    added slider functionality

--- js/partners.js
+++ js/partners.js
@@ -1,12 +1,33 @@
 $( document ).ready(function() {
 
     // Your code here.
+    $("#helpingHands").show();
     $('.backsplash ul li').click(function() {
     var i = $(this).index();
     $('.bottomsection').hide();
     $('#section' + (i+1)).show();
     $(this).siblings('li').css("fontWeight", "normal");
     $(this).css("fontWeight", "bold");
+    });
+
+    document.getElementById("myRange").addEventListener("input",
+    function() {
+        $('.sliderSection').hide();
+        let val = $(this).val();
+        if (val < 20) {
+            $("#helpingHands").show();
+        } else if (val < 40) {
+            $("#goodNeighbor").show();
+        } else if (val < 60) {
+            $("#handyman").show();
+        } else if (val < 80) {
+            $("#foreman").show();
+        } else if(val < 100) {
+            $("#generalContractor").show();
+        } else {
+            $("#blockRenovator").show();
+        }
+
     });
 
 });
